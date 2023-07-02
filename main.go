029c00a179987c7815399c7c79785786319439d8
Harish Sundaresh <yesharish@yahoo.in>
2023-07-02 10:56:30 +1000

    Check whether NativeMemoryTracking is enabled for a given Java PID

--- main.go
+++ main.go
@@ -4,24 +4,50 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
+	"regexp"
+	"strings"
 )
 
 func identifyJavaPid() string {
-	app := "pgrep"
+	command := "pgrep"
 	arg0 := "-o"
 	arg1 := "java"
 
-	cmd := exec.Command(app, arg0, arg1)
+	cmd := exec.Command(command, arg0, arg1)
 	stdout, err := cmd.Output()
 
 	if err != nil {
 		log.Fatal(err.Error())
 		log.Fatal("No Java PID found!")
 	}
-	return string(stdout)
+	return strings.ReplaceAll(string(stdout), "\n", "")
+}
+
+func checkNMTEnabled(pid string) bool {
+
+	command := "ps"
+	arg0 := "-p"
+	arg1 := pid
+	arg2 := "-o"
+	arg3 := "args"
+	arg4 := "--no-headers"
+
+	cmd := exec.Command(command, arg0, arg1, arg2, arg3, arg4)
+
+	stdout, err := cmd.Output()
+
+	if err != nil {
+		log.Fatal(err.Error())
+		log.Fatal("NativeMemoryTracking not enabled for PID: " + pid)
+	}
+
+	match, _ := regexp.MatchString("NativeMemoryTracking", string(stdout))
+	return match
 }
 
 func main() {
-
-	fmt.Printf("Java PID identified: %s", identifyJavaPid())
+	javaPid := identifyJavaPid()
+	fmt.Printf("Java PID identified: %s", javaPid)
+	match := checkNMTEnabled(javaPid)
+	fmt.Println(match)
 }
